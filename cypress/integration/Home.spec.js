b5169764cd36b62ce07bcb934390df954ab9c494
Se-Gl <me@severinglaser.ch>
2021-11-30 22:11:32 +0100

    adjusted responsive hero section

--- cypress/integration/Home.spec.js
+++ cypress/integration/Home.spec.js
@@ -3,13 +3,13 @@ describe('Home Screen Unit test', () => {
     cy.visit('http://localhost:3000')
   })
   it('renders the logo', () => {
-    cy.get('.hero nav').contains('img')
+    cy.get('.logo').contains('img')
     cy.get('#logo').should('have.attr', 'alt').should('not.be.empty').and('contain', 'OmenCSS Logo')
   })
   it('renders the header', () => {
     cy.get('.center h1').contains('Writing CSS reimagined', { matchCase: false })
   })
   it('shows animated bubbles', () => {
-    cy.get('.sticky .bubbles .bubble').should('be.visible')
+    cy.get('.bubbles .bubble').should('be.visible')
   })
 })
