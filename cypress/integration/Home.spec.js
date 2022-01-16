90b8e8ccd4554d5011218c92f1cac6b8bff6a7af
Se-Gl <me@severinglaser.ch>
2022-01-16 20:23:53 +0100

    updated dependencies, added omenCSS

--- cypress/integration/Home.spec.js
+++ cypress/integration/Home.spec.js
@@ -3,11 +3,11 @@ describe('Home Screen Unit test', () => {
     cy.visit('http://localhost:3000')
   })
   it('renders the logo', () => {
-    cy.get('.logo').contains('img')
+    cy.get('nav div').contains('img')
     cy.get('#logo').should('have.attr', 'alt').should('not.be.empty').and('contain', 'OmenCSS Logo')
   })
   it('renders the header', () => {
-    cy.get('.center h1').contains('Writing CSS reimagined', { matchCase: false })
+    cy.get('main div h1').contains('Writing CSS reimagined', { matchCase: false })
   })
   it('shows animated bubbles', () => {
     cy.get('.bubbles .bubble').should('be.visible')
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import '../styles/globals.scss'
+import '../styles/omencss.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
