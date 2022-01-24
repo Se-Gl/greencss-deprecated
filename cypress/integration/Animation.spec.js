ae87800b2dd57f39ca7771cc40cefbfd449614a5
Se-Gl <me@severinglaser.ch>
2022-01-24 23:08:19 +0100

    added animation page

---
+++ cypress/integration/Animation.spec.js
@@ -0,0 +1,16 @@
+describe('Home Screen Unit test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/animation')
+  })
+  it('renders the custom input select', () => {
+    cy.get('#custom-dropdown').should('be.visible').should('exist')
+  })
+  it('show all animations', () => {
+    cy.get('#custom-dropdown').click()
+  })
+  it('clicks the first animation', () => {
+    cy.get('#animation-null').should('exist').should('be.visible')
+    cy.get('#custom-dropdown').click().get('.absolute > :nth-child(1)').click()
+    cy.get('#animation-jump').should('exist').should('be.visible')
+  })
+})
---
+++ data/animations.js
@@ -0,0 +1,16 @@
+export const animations = [
+  { id: 1, label: 'Jump', value: 'jump' },
+  { id: 2, label: 'Flash', value: 'flash' },
+  { id: 3, label: 'Poke From Top', value: 'poke-top' },
+  { id: 4, label: 'Poke From Right', value: 'poke-right' },
+  { id: 5, label: 'Poke From Bottom', value: 'poke-bottom' },
+  { id: 6, label: 'Poke From Left', value: 'poke-left' },
+  { id: 7, label: 'Rotate In Clockwise', value: 'rotate-in-left' },
+  { id: 8, label: 'Rotate In Anti Clockwise', value: 'rotate-in-right' },
+  { id: 9, label: 'Rotate Out Clockwise', value: 'rotate-out-left' },
+  { id: 10, label: 'Rotate Out Anti Clockwise', value: 'rotate-out-right' },
+  { id: 11, label: 'Reveal From Top', value: 'reveal-down' },
+  { id: 12, label: 'Reveal From Left', value: 'reveal-left' },
+  { id: 13, label: 'Reveal From Right', value: 'reveal-right' },
+  { id: 14, label: 'Reveal From Bottom', value: 'reveal-up' }
+]
