97fda7f85703bf97f2cd6327cd8b9426979cec87
Se-Gl <me@severinglaser.ch>
2022-02-09 21:52:21 +0100

    added editor tests

--- cypress/integration/Editor.spec.js
+++ cypress/integration/Editor.spec.js
@@ -1,13 +1,15 @@
 describe('Editor Screen Unit test', () => {
   before(() => {
     cy.visit('http://localhost:3000/editor')
+    cy.exec('rm -f cypress/downloads/*', { log: true, failOnNonZeroExit: false })
   })
   it('add dummy text', () => {
     cy.get('.w-md-editor-text-input').type('# test title')
     cy.get('#user-content-test-title').should('exist')
   })
-  it('download button exists', () => {
-    cy.get('#saveMD').should('exist')
+  it('download button exists and opens myFile.md when clicked', () => {
+    cy.get('#saveMD').should('exist').click().wait(2000)
+    cy.readFile('cypress/downloads/myFile.md').should('exist')
   })
   it('includes layout component', () => {
     cy.get('nav').should('exist')
