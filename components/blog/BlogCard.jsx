3bcb544fc74c3354d8b0c85f1a28c962581f00b8
Se-Gl <me@severinglaser.ch>
2022-02-10 21:27:58 +0100

    adjusted blog card colouring, added backup background colour

--- components/blog/BlogCard.jsx
+++ components/blog/BlogCard.jsx
@@ -10,30 +10,31 @@ export default function BlogCard({ post }) {
   const vibrantColor = data.vibrant
   const color = tinycolor(`${vibrantColor}`)
   const isDark = color.isDark()
+  console.log(vibrantColor)
 
   return (
-    <div className='col-span-1 grid-flow-row sm:m-10px md:m-10px' id={`${post.frontmatter.title}`}>
+    <div className='col-span-1 grid-flow-row sm:m-10px md:m-10px' id={`${post.slug}`}>
       {loading ? (
         <Loader />
       ) : (
         <div
           className={`min-h-50rem w-100per rounded-10px bg-cover bg-no-repeat bg-center ${
-            !isDark && 'border-1px border-solid border-dark'
+            isDark && 'border-1px border-solid border-dark'
           }`}
-          style={{ backgroundImage: `url(${post.frontmatter.cover_image})` }}
+          style={{ backgroundImage: `url(${post.frontmatter.cover_image})`, backgroundColor: `${vibrantColor}` }}
           id='bg-image'>
           <div className='flex h-50rem min-h-50per'>
             <Link href={`/blog/${post.slug}`}>
               <div
                 style={{ cursor: 'pointer' }}
-                className={`relative m-auto max-w-75per w-75per min-h-80per ${isDark && 'bg-light text-dark'} ${
-                  !isDark && 'bg-dark text-light'
+                className={`relative m-auto max-w-75per w-75per min-h-80per ${!isDark && 'bg-light text-dark'} ${
+                  isDark && 'bg-dark text-light'
                 }
               `}>
                 <h1 className='text-50px font-bold leading-100per'>{post.frontmatter.title}</h1>
                 <p className='text-25px font-normal mt-25px'>{post.frontmatter.excerpt}</p>
                 <LinkButton
-                  className={`absolute bottom-0 mt-50px ${isDark && 'text-dark'} ${!isDark && 'text-light'}`}
+                  className={`absolute bottom-0 mt-50px ${!isDark && 'text-dark'} ${isDark && 'text-light'}`}
                   href={`/blog/${post.slug}`}>
                   Read more
                 </LinkButton>
