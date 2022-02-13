940dd872f761f00b29e6fb76ce65134fd777a8e4
Se-Gl <me@severinglaser.ch>
2022-02-13 17:38:54 +0100

    fixed testimonial section

--- components/LandingPage/Testimonial.jsx
+++ components/LandingPage/Testimonial.jsx
@@ -2,16 +2,16 @@ import Image from 'next/image'
 
 export default function Testimonial() {
   return (
-    <div className='my-20rem sm:my-100px md:my-100px' id='testimonial'>
+    <section className='my-20rem sm:my-100px md:my-100px' id='testimonial'>
       <div className='relative flex min-h-75vh w-100per text-center text-light bg-dark rounded-20px'>
         <div className='m-auto max-w-50rem'>
           <h1 className='font-bold text-50px mb-50px leading-120per'>Hear What The Creator Says</h1>
-          <p className='text-25px font-normal mt-25px'>
+          <p className='text-25px font-normal mt-25px text-light'>
             “A dynamic, animated CSS library. Beginner-friendly, for every framework or plain HTML, intuitive and you
             can easily make it lightweight.”
           </p>
           <p className='text-25px font-normal text-purple-3 mt-50px'>Severin Glaser</p>
-          <p className='text-25px font-normal'>omenCSS</p>
+          <p className='text-25px font-normal text-light'>omenCSS</p>
         </div>
 
         <div className='absolute left-0' style={{ top: '-10%' }}>
@@ -32,7 +32,7 @@ export default function Testimonial() {
             id='creator-memoji-2'
           />
         </div>
-        <div className='absolute right-0' style={{ bottom: '-1%' }}>
+        <div className='absolute right-0 sm:hidden' style={{ bottom: '-1%' }}>
           <Image
             src='/images/landingpage/bottom-right-min.png'
             alt='Severin Glaser - Memoji 3'
@@ -41,7 +41,7 @@ export default function Testimonial() {
             id='creator-memoji-3'
           />
         </div>
-        <div className='absolute left-0 mt-50px' style={{ bottom: '-1.3%' }}>
+        <div className='absolute left-0 mt-50px sm:hidden' style={{ bottom: '-1.3%' }}>
           <Image
             src='/images/landingpage/bottom-left-min.png'
             alt='Severin Glaser - Memoji 4'
@@ -51,6 +51,6 @@ export default function Testimonial() {
           />
         </div>
       </div>
-    </div>
+    </section>
   )
 }
