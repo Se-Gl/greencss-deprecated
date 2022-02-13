0f471cb4829390a5dacc4a051250b8efe45566a5
Se-Gl <me@severinglaser.ch>
2022-02-13 23:42:37 +0100

    refactored getPosts

--- pages/blog/page/[page_index].jsx
+++ pages/blog/page/[page_index].jsx
@@ -1,12 +1,11 @@
 import fs from 'fs'
 import path from 'path'
-import matter from 'gray-matter'
 import BlogLayout from '@/components/blog/BlogLayout'
 import Layout from '@/components/reusable/Layout'
 import BlogCard from '@/components/blog/BlogCard'
-import { sortByDate } from '@/utils/SortByDate'
 import { POSTS_PER_PAGE } from '@/config/index'
 import Pagination from '@/components/reusable/Pagination'
+import { getPosts } from '@/lib/posts'
 
 export default function BlogIndex({ posts, numPages, currentPage }) {
   return (
@@ -49,20 +48,11 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   const page = parseInt((params && params.page_index) || 1)
   const files = fs.readdirSync(path.join('posts'))
-  const posts = files.map((filename) => {
-    const slug = filename.replace('.md', '')
-    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
-    const { data: frontmatter } = matter(markdownWithMeta)
-
-    return {
-      slug,
-      frontmatter
-    }
-  })
+  const posts = getPosts()
 
   const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
   const pageIndex = page - 1
-  const orderedPosts = posts.sort(sortByDate).slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE)
+  const orderedPosts = posts.slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE)
 
   return {
     props: {
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,3 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
 import Hero from '@/components/LandingPage/Hero'
 import News from '@/components/LandingPage/News'
 import Presentation from '@/components/LandingPage/Presentation'
@@ -8,8 +5,8 @@ import Service from '@/components/LandingPage/Service'
 import Support from '@/components/LandingPage/Support'
 import Testimonial from '@/components/LandingPage/Testimonial'
 import Bubbles from '@/components/reusable/Bubbles'
-import { sortByDate } from '@/utils/SortByDate'
 import Layout from '@/components/reusable/Layout'
+import { getPosts } from '@/lib/posts'
 
 export default function HomePage({ posts }) {
   return (
@@ -28,23 +25,9 @@ export default function HomePage({ posts }) {
 }
 
 export async function getStaticProps() {
-  const files = fs.readdirSync(path.join('posts'))
-
-  const posts = files.map((filename) => {
-    const slug = filename.replace('.md', '')
-
-    const markdownMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
-    const { data: frontmatter } = matter(markdownMeta)
-
-    return {
-      slug,
-      frontmatter
-    }
-  })
-
   return {
     props: {
-      posts: posts.sort(sortByDate).slice(0, 3)
+      posts: getPosts().slice(0, 3)
     }
   }
 }
