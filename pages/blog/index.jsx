138e344d26a5a882ef0b52f5b8c8c950b66c48b9
Se-Gl <me@severinglaser.ch>
2022-02-13 22:28:23 +0100

    added pagination

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,49 +1,5 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import BlogLayout from '../../components/blog/BlogLayout'
-import Layout from '../../components/reusable/Layout'
-import BlogCard from '../../components/blog/BlogCard'
-import { sortByDate } from '../../utils/SortByDate'
+import { getStaticProps } from './page/[page_index]'
+import BlogPage from './page/[page_index]'
 
-export default function BlogIndex({ posts }) {
-  return (
-    <Layout className='flex container sm:px-10px md:px-25px lg:px-50px min-h-100vh mb-10rem'>
-      <div className='min-w-100per relative'>
-        <div className='m-auto max-w-50rem mb-10rem'>
-          <h1 className='font-bold text-50px mb-50px leading-120per'>A Blog - the right way to tell a story</h1>
-          <p className='text-25px font-normal mt-25px'>
-            Explore the blog posts, to learn more about the latest tips and tricks.
-          </p>
-        </div>
-        <BlogLayout>
-          {posts.map((post, index) => (
-            <BlogCard key={index} post={post} index={index} />
-          ))}
-        </BlogLayout>
-      </div>
-    </Layout>
-  )
-}
-
-export async function getStaticProps() {
-  const files = fs.readdirSync(path.join('posts'))
-
-  const posts = files.map((filename) => {
-    const slug = filename.replace('.md', '')
-
-    const markdownMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
-    const { data: frontmatter } = matter(markdownMeta)
-
-    return {
-      slug,
-      frontmatter
-    }
-  })
-
-  return {
-    props: {
-      posts: posts.sort(sortByDate)
-    }
-  }
-}
+export { getStaticProps }
+export default BlogPage
---
+++ pages/blog/page/[page_index].jsx
@@ -0,0 +1,72 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import BlogLayout from '../../../components/blog/BlogLayout'
+import Layout from '../../../components/reusable/Layout'
+import BlogCard from '../../../components/blog/BlogCard'
+import { sortByDate } from '../../../utils/SortByDate'
+import { POSTS_PER_PAGE } from '../../../config'
+
+export default function BlogIndex({ posts, numPages, currentPage }) {
+  return (
+    <Layout className='flex container sm:px-10px md:px-25px lg:px-50px min-h-100vh mb-10rem'>
+      <div className='min-w-100per relative'>
+        <div className='m-auto max-w-50rem mb-10rem'>
+          <h1 className='font-bold text-50px mb-50px leading-120per'>A Blog - the right way to tell a story</h1>
+          <p className='text-25px font-normal mt-25px'>
+            Explore the blog posts, to learn more about the latest tips and tricks.
+          </p>
+        </div>
+        <BlogLayout>
+          {posts.map((post, index) => (
+            <BlogCard key={index} post={post} index={index} />
+          ))}
+        </BlogLayout>
+      </div>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join('posts'))
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
+  let paths = []
+
+  for (let i = 1; i <= numPages; i++) {
+    paths.push({
+      params: { page_index: i.toString() }
+    })
+  }
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const page = parseInt((params && params.page_index) || 1)
+  const files = fs.readdirSync(path.join('posts'))
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '')
+    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter
+    }
+  })
+
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
+  const pageIndex = page - 1
+  const orderedPosts = posts.sort(sortByDate).slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE)
+
+  return {
+    props: {
+      posts: orderedPosts,
+      numPages,
+      currentPage: page
+    }
+  }
+}
