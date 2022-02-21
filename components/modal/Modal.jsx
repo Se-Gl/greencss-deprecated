7b3d30a99954512cc3f6c0678a095b85770a337d
Se-Gl <me@severinglaser.ch>
2022-02-21 20:13:27 +0100

    adjusted responsive modal values

--- components/modal/Modal.jsx
+++ components/modal/Modal.jsx
@@ -26,10 +26,10 @@ const Modal = ({ show, onClose, children, title = 'Search documentation' }) => {
 
   const modalContent = show ? (
     <div
-      className='fade-in animation-duration-300ms animation-forwards absolute top-0per left-0per w-100per h-100per flex justify-center items-center '
+      className='fade-in animation-duration-300ms animation-forwards absolute top-0per left-0per w-100per h-100per flex justify-center items-center'
       style={{ backgroundColor: 'rgba(16,16,16,0.5)' }}>
       <div className='relative z-1 w-100vw h-100vh' onClick={handleCloseClick} id='backdrop-close' />
-      <div className='absolute z-2 bg-white max-w-50rem w-50rem min-h-50rem rounded-10px'>
+      <div className='absolute z-2 bg-white max-w-50rem w-50rem sm:w-100per min-h-50rem sm:min-h-75vh rounded-10px px-25px sm:px-10px'>
         <div className='flex justify-end'>
           <a
             href='#'
