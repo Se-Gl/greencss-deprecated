0e968a64a85ecb0f3c241de48574ebd45d76ae74
Se-Gl <me@severinglaser.ch>
2022-02-24 12:05:58 +0100

    added dynamic progress loading bar

---
+++ components/progress/Progress.jsx
@@ -0,0 +1,58 @@
+// idea from: https://sidneyliebrand.io/blog/building-a-next-js-page-load-progress-bar
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+
+export default function Progress() {
+  const router = useRouter()
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+    let progressTime
+    function start() {
+      setProgress(1)
+      incrementValue()
+    }
+
+    function incrementValue() {
+      const timeout = Math.round(Math.random() * 300)
+
+      setProgress((progressCalc) => {
+        const percent = Math.round(Math.random() * 10)
+        const next = Math.min(progressCalc + percent, 80)
+
+        if (next < 80) {
+          progressTime = setTimeout(incrementValue, timeout)
+          return next
+        }
+        return 80
+      })
+    }
+
+    function checkComplete() {
+      clearTimeout(progressTime)
+      setProgress(100)
+    }
+    router.events.on('routeChangeStart', start)
+    router.events.on('routeChangeComplete', checkComplete)
+    router.events.on('routeChangeError', checkComplete)
+
+    return () => {
+      clearTimeout(progressTime)
+      router.events.off('routeChangeStart')
+      router.events.off('routeChangeComplete')
+      router.events.off('routeChangeError')
+    }
+  }, [])
+
+  return (
+    <div className='fixed top-0per left-0per z-99 h-5px w-100per' id='progress-loader'>
+      <div
+        className='bg-purple absolute top-0per right-0per bottom-0per left-0per w-0px transition-all transition-duration-500ms transition-opacity'
+        style={{
+          width: `${progress}%`,
+          opacity: progress > 0 && progress < 100 ? 1 : 0
+        }}
+      />
+    </div>
+  )
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,15 @@
 import '@/styles/omencss.css'
 import '@/styles/globals.scss'
 
+import Progress from '@/components/progress/Progress'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Progress />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
