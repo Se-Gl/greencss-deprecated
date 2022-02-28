231061eb79e37100d8eae9d63da61f097b5047a0
Se-Gl <me@severinglaser.ch>
2022-02-28 20:49:57 +0100

    display all subcategories on docs index page

--- pages/docs/page/[page_index].jsx
+++ pages/docs/page/[page_index].jsx
@@ -6,13 +6,13 @@ import { getPosts } from '@/lib/posts'
 import BrandCard from '@/components/brand/BrandCard'
 import SideBar from '@/components/category/SideBar'
 
-export default function CategoryPage({ categories }) {
+export default function CategoryPage({ categories, posts }) {
   return (
     <Layout>
       <div className='grid grid-col-6 gap-30px'>
         <div className='grid-col-1 col-span-1 min-h-100vh sm:display-none md:display-none' id='sidebar'>
           <ul>
-            <SideBar categories={categories} />
+            <SideBar categories={categories} subcategories={posts} hasSubcategory={true} />
           </ul>
         </div>
         <div className='min-w-100per relative col-span-5 sm:col-span-6 md:col-span-6' id='docs-index'>
@@ -60,11 +60,10 @@ export async function getStaticProps({ params }) {
   const posts = getPosts()
   const categories = posts.map((post) => post.frontmatter.category)
   const uniqueCategories = [...new Set(categories)]
-  const categoryPosts = posts.filter((post) => post.frontmatter.category.toLowerCase() === params)
 
   return {
     props: {
-      posts: categoryPosts,
+      posts,
       categories: uniqueCategories
     }
   }
