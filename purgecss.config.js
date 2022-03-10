96efcbe47c6f3d1dcee19f538a51651cad97941e
Se-Gl <me@severinglaser.ch>
2022-03-10 18:18:04 +0100

    fixed purgecss not updating new classNames

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,50 +1,7 @@
-// module.exports = {
-//   content: [
-//     '*.{js,jsx,ts,tsx}',
-//     'components/*.{js,jsx,ts,tsx}',
-//     'components/**/*.{js,jsx,ts,tsx}',
-//     'components/**/**/*.{js,jsx,ts,tsx}',
-//     'components/**/**/**/*.{js,jsx,ts,tsx}',
-//     'pages/*.{js,jsx,ts,tsx}',
-//     'pages/**/*.{js,jsx,ts,tsx}',
-//     'pages/**/**/*.{js,jsx,ts,tsx}',
-//     'pages/**/**/**/*.{js,jsx,ts,tsx}'
-//   ],
-//   css: ['styles/omencss.css'],
-//   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-//   output: 'css/omen.css'
-// }
-
 module.exports = {
-  plugins: [
-    process.env.NODE_ENV === 'production'
-      ? [
-          'postcss-flexbugs-fixes',
-          [
-            'postcss-preset-env',
-            {
-              autoprefixer: {
-                flexbox: 'no-2009'
-              },
-              stage: 2,
-              features: {
-                'custom-properties': false
-              }
-            }
-          ],
-          [
-            '@fullhuman/postcss-purgecss',
-            {
-              content: ['pages/**/*.{js,jsx,ts,tsx}', 'components/**/*.{js,jsx,ts,tsx}', './data/**/*.{js,jsx,ts,tsx}'],
-              css: ['styles/omencss.css'],
-              defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-              output: 'css/omen.css',
-              safelist: ['html', 'body']
-            }
-          ]
-        ]
-      : [
-          // No transformations in development
-        ]
-  ]
+  content: ['pages/**/*.{js,jsx,ts,tsx}', 'components/**/*.{js,jsx,ts,tsx}'],
+  css: ['./styles/omencss.css'],
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  safelist: ['html', 'body'],
+  output: './css/omen.css'
 }
