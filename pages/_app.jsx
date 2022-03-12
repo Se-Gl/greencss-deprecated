7bc3b47d8b9a3d2139f562b0d7c2a6845224f294
Se-Gl <me@severinglaser.ch>
2022-03-12 17:55:00 +0100

    added PostHog for analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import { usePostHog } from 'next-use-posthog'
 // import '@/styles/globals.scss'
 if (process.env.NODE_ENV == 'production') {
   require('@/css/omen.css')
@@ -11,6 +12,13 @@ import { ToastProvider } from '@/components/toast/context/ToastContext'
 import ToastContainer from '@/components/toast/ToastContainer'
 
 function MyApp({ Component, pageProps }) {
+  usePostHog(`${process.env.POSTHOG_KEY}`, {
+    api_host: 'https://app.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing()
+    }
+  })
+
   return (
     <>
       <Progress />
