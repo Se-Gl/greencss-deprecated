740d3033e4aa575823d6aa2987a2a2c5da0d300c
Se-Gl <me@severinglaser.ch>
2022-03-21 00:27:21 +0100

    adjusted presentation section

--- components/LandingPage/Presentation.jsx
+++ components/LandingPage/Presentation.jsx
@@ -1,79 +1,48 @@
-import React from 'react'
 import useInView from '@/hooks/InView/scrollView'
-import CLine from './CLine'
-import SLine from './SLine'
 
 export default function Presentation() {
   const [ref, isVisible] = useInView({
-    threshold: 0.25,
+    threshold: 0,
     unobserveOnEnter: true
   })
-  const [refTwo, isVisibleTwo] = useInView({
-    threshold: 0.25,
-    unobserveOnEnter: true
-  })
-  const [refThree, isVisibleThree] = useInView({
-    threshold: 0.25,
-    unobserveOnEnter: true
-  })
+
+  const serviceCard = [
+    {
+      title: 'Animated',
+      description:
+        'Pure CSS animations work with vanilla HTML or with any web framework - from JavaScript over Python and even PHP.'
+    },
+    {
+      title: 'Responsive',
+      description:
+        'Design your website for all users, whether desktop, laptop, tablet or smartphone. Modify layouts comfortably and intuitively.'
+    },
+    {
+      title: 'Production',
+      description: 'Exclude unused CSS classes in order to reduce loading times and increase best practices.'
+    }
+  ]
 
   return (
     <section className='my-20rem sm:my-0px md:my-0px' id='presentation'>
       <div className='m-auto grid grid-col-3 gap-30px sm:gap-0px sm:grid-col-1 md:grid-col-1 lg:grid-col-2'>
-        <div
-          ref={ref}
-          id='animated'
-          className={`col-span-1 grid-flow-row sm:mx-10px md:mx-10px ${
-            isVisible ? 'fade-in animate animation-forwards' : 'opacity-0per'
-          }`}>
-          <div className='flex'>
-            <div className='mt-35px' style={{ minHeight: '228px', minWidth: '10px' }}>
-              <CLine />
-            </div>
-            <div className='ml-10px'>
-              <h2>Animated</h2>
-              <p>
-                Pure CSS animations work with vanilla HTML or with any web framework - from JavaScript over Python and
-                even PHP.
-              </p>
+        {serviceCard.sort().map((card, index) => (
+          <div key={index} ref={ref}>
+            <div
+              style={{ opacity: 0 }}
+              className={`min-h-50rem w-100per rounded-10px bg-cover bg-no-repeat bg-center border-1px border-solid border-dark ${
+                isVisible && `fade-in animate animation-forwards animation-delay-${(index + 1) * 2}00ms`
+              }`}
+              id={`${card.title.toLowerCase()}`}>
+              <div className='flex h-50rem min-h-50per'>
+                <div className='relative m-auto max-w-75per w-75per min-h-80per bg-dark text-white'>
+                  <h2 className='text-45px'>{card.title}</h2>
+                  <p className='text-black-5'>{card.description}</p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div
-          ref={refTwo}
-          id='responsive'
-          className={`col-span-1 grid-flow-row sm:mx-10px md:mx-10px ${
-            isVisibleTwo ? 'fade-in animate animation-forwards animation-delay-200ms' : 'opacity-0per'
-          }`}>
-          <div className='flex'>
-            <div className='mt-35px' style={{ minHeight: '228px', minWidth: '11px' }}>
-              <SLine />
-            </div>
-            <div className='ml-10px'>
-              <h2>Responsive</h2>
-              <p>
-                Design your website for all users, whether desktop, laptop, tablet or smartphone. Modify layouts
-                comfortably and intuitively.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div
-          ref={refThree}
-          id='production'
-          className={`col-span-1 grid-flow-row sm:mx-10px md:mx-10px ${
-            isVisibleThree ? 'fade-in animate animation-forwards animation-delay-400ms' : 'opacity-0per'
-          }`}>
-          <div className='flex'>
-            <div className='mt-35px' style={{ minHeight: '228px', minWidth: '11px' }}>
-              <SLine />
-            </div>
-            <div className='ml-10px'>
-              <h2>Production</h2>
-              <p>Unused CSS classes will automatically be excluded. Reduce loading times, increase best practices.</p>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   )
