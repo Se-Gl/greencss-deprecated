0e392dceefd9b888325bd5c6fd9579b865633455
Se-Gl <me@severinglaser.ch>
2022-04-07 16:35:57 +0200

    adjusted testimonial section

--- components/LandingPage/Testimonial.jsx
+++ components/LandingPage/Testimonial.jsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Testimonial() {
@@ -8,50 +7,13 @@ export default function Testimonial() {
         <div className='m-auto max-w-50rem'>
           <h2 className='font-bold text-50px mb-50px'>Hear What The Creator Says</h2>
           <p className='text-white'>
-            “A dynamic, animated CSS library. Beginner-friendly, for every framework or plain HTML, intuitive and you
-            can easily make it lightweight.”
+            “A dynamic, animated CSS library. Beginner-friendly, for every framework or plain HTML, intuitive,
+            development ready and you can easily make it lightweight in production.”
           </p>
           <Link href='https://www.severinglaser.ch'>
-            <a className='text-purple-3 mt-50px no-decoration'>Severin Glaser</a>
+            <a className='text-purple mt-50px no-decoration'>Severin Glaser</a>
           </Link>
-          <p className='text-white'>omenCSS</p>
-        </div>
-
-        <div className='absolute left-0per' style={{ top: '-10%' }}>
-          <Image
-            src='/images/landingpage/top-left-min.png'
-            alt='Severin Glaser - Memoji 1'
-            width={200}
-            height={228.45}
-            id='creator-memoji-1'
-          />
-        </div>
-        <div className='absolute right-0per' style={{ top: '-10%' }}>
-          <Image
-            src='/images/landingpage/top-right-min.png'
-            alt='Severin Glaser - Memoji 2'
-            width={200}
-            height={200}
-            id='creator-memoji-2'
-          />
-        </div>
-        <div className='absolute right-0per sm:hidden' style={{ bottom: '-1%' }}>
-          <Image
-            src='/images/landingpage/bottom-right-min.png'
-            alt='Severin Glaser - Memoji 3'
-            width={200}
-            height={231.18}
-            id='creator-memoji-3'
-          />
-        </div>
-        <div className='absolute left-0per mt-50px sm:hidden' style={{ bottom: '-1.3%' }}>
-          <Image
-            src='/images/landingpage/bottom-left-min.png'
-            alt='Severin Glaser - Memoji 4'
-            width={200}
-            height={199.64}
-            id='creator-memoji-4'
-          />
+          <p className='text-white mb-0px'>omenCSS</p>
         </div>
       </div>
     </section>
