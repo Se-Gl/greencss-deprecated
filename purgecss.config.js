b6b207dc122b00f02d92c938fea34aceed15361c
Se-Gl <me@severinglaser.ch>
2022-04-13 02:04:25 +0200

    adjusted CopyIcon to be visible in production

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['pages/**/*.{js,jsx,ts,tsx}', 'components/**/*.{js,jsx,ts,tsx}'],
+  content: ['pages/**/*.{js,jsx,ts,tsx}', 'components/**/*.{js,jsx,ts,tsx}', 'utils/**/*.{js,jsx,ts,tsx}'],
   css: ['./styles/omencss.css'],
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   safelist: ['html', 'body'],
