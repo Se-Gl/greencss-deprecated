f1e0088fa9b6b83fde028ef8796dae5b5bf9cdf7
Se-Gl <me@severinglaser.ch>
2022-04-18 22:55:25 +0200

    updated chevron fill color

--- components/icon/ChevronRightHover.jsx
+++ components/icon/ChevronRightHover.jsx
@@ -9,11 +9,11 @@ function ChevronRightHover({ className }) {
       className={className}>
       <path
         className='clip-circle-in animation-duration-1000ms animation-infinite animation-alternate-reverse animation-delay-100ms'
-        fill='#8940FF'
+        fill='#62c203'
         d='M8.893 14.291a1.007 1.007 0 010-1.42l5.293-5.315L8.893 2.24a1.007 1.007 0 010-1.42.997.997 0 011.414 0l6 6.025c.39.392.39 1.028 0 1.42l-6 6.025a.997.997 0 01-1.414 0z'></path>
       <path
         className='clip-circle-in animation-duration-1000ms animation-infinite animation-alternate-reverse'
-        fill='#6100FF'
+        fill='#3e7a02'
         d='M.893 14.291a1.007 1.007 0 010-1.42l5.293-5.315L.893 2.24a1.007 1.007 0 010-1.42.997.997 0 011.414 0l6 6.025c.39.392.39 1.028 0 1.42l-6 6.025a.997.997 0 01-1.414 0z'></path>
     </svg>
   )
