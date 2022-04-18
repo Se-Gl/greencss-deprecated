b4ab38fbbcfcff421b80cdd352d66789dec7185e
Se-Gl <me@severinglaser.ch>
2022-04-18 21:33:15 +0200

    adjusted search results color

--- components/icon/ChevronRight.jsx
+++ components/icon/ChevronRight.jsx
@@ -2,7 +2,7 @@ function ChevronRight({ className }) {
   return (
     <svg xmlns='http://www.w3.org/2000/svg' width='8' height='15' fill='none' viewBox='0 0 8 15' className={className}>
       <path
-        fill='#6100FF'
+        fill='#3e7a02'
         d='M.293 13.765a1.007 1.007 0 010-1.42L5.586 7.03.293 1.714a1.007 1.007 0 010-1.42.997.997 0 011.414 0l6 6.025c.39.393.39 1.028 0 1.42l-6 6.026a.997.997 0 01-1.414 0z'></path>
     </svg>
   )
