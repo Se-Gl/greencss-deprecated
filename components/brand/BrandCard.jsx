a05e92bf169620ad16055308463d45cf5038bfe9
Se-Gl <me@severinglaser.ch>
2022-04-20 23:24:12 +0200

    adjusted BrandCard design

--- components/brand/BrandCard.jsx
+++ components/brand/BrandCard.jsx
@@ -7,13 +7,10 @@ export default function BrandCard({ title, link }) {
       <div className='min-h-25rem w-100per rounded-10px bg-cover bg-no-repeat bg-center border-1px border-solid border-dark'>
         <div className='flex h-25rem min-h-50per'>
           <Link href={link} passHref>
-            <div
-              style={{ cursor: 'pointer' }}
-              className={`relative m-auto max-w-75per w-75per min-h-80per bg-black-10 text-black'}
-  `}>
+            <div className='relative m-auto max-w-75per w-75per min-h-80per bg-black cursor-pointer'>
               <div className='p-20px'>
-                <h3 className='text-left text-30px'>{title}</h3>
-                <LinkButton className='absolute bottom-10per' href={link} id={`brand-redirect-${title}`}>
+                <h3 className='text-left text-25px text-white'>{title}</h3>
+                <LinkButton className='absolute bottom-10per text-white' href={link} id={`brand-redirect-${title}`}>
                   Read more
                 </LinkButton>
               </div>
