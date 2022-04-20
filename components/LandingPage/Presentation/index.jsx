566c359c3c319b8a1a9fb2d143aea2355bebd52e
Se-Gl <me@severinglaser.ch>
2022-04-20 23:16:45 +0200

    refactored presentation cards, added new styling

--- components/LandingPage/Presentation/index.jsx
+++ components/LandingPage/Presentation/index.jsx
@@ -1,31 +1,35 @@
+import dynamic from 'next/dynamic'
 import Image from 'next/image'
-import useInView from '@/hooks/InView/scrollView'
-import Section from '../reusable/Section'
+import Loader from '@/components/logo/Loader'
+import Section from '../../reusable/Section'
+
+const PresentationCard = dynamic(() => import('./PresentationCard'), { loading: () => <Loader /> })
+const CompetitiveIcon = dynamic(() => import('@/components/icon/Presentation/Competitive'))
+const ResponsiveIcon = dynamic(() => import('@/components/icon/Presentation/Responsive'))
+const AnimatedIcon = dynamic(() => import('@/components/icon/Presentation/Animated'))
 
 export default function Presentation() {
-  const [ref, isVisible] = useInView({
-    threshold: 0,
-    unobserveOnEnter: true
-  })
-
   const serviceCard = [
     {
       title: 'Animated',
       description:
         'Pure CSS animations work with vanilla HTML or with any web framework - from JavaScript over Python and even PHP.',
-      bgcolour: 'bg-green-9'
+      bgcolour: 'bg-green-9',
+      icon: <AnimatedIcon />
     },
     {
       title: 'Responsive',
       description:
         'Design your website for all users, whether desktop, laptop, tablet or smartphone. Comfortably and intuitively and easy to learn - like bulma or tailwindcss but for everyone.',
-      bgcolour: 'bg-yellow-9'
+      bgcolour: 'bg-yellow-9',
+      icon: <ResponsiveIcon />
     },
     {
       title: 'Competitive',
       description:
-        'Created like bulma with SASS (SCSS). Classic like tailwindcss, but more intuitive. Or simply for you and your planet.',
-      bgcolour: 'bg-blue-9'
+        'Created like bulma with SASS (SCSS). Classic like tailwindcss, but more intuitive. For the environment and for yourself - do something good while you work.',
+      bgcolour: 'bg-blue-9',
+      icon: <CompetitiveIcon />
     }
   ]
 
@@ -54,21 +58,7 @@ export default function Presentation() {
         </div>
       </div>
       <div className='m-auto grid grid-col-3 gap-30px sm:gap-0px sm:grid-col-1 md:grid-col-2 lg:grid-col-2 my-auto'>
-        {serviceCard.sort().map((card, index) => (
-          <div
-            key={index}
-            ref={ref}
-            style={{ opacity: 0 }}
-            className={`col-span-1 sm:mt-25px w-100per rounded-10px border-1px border-solid border-dark min-h-30rem flex ${
-              card.bgcolour
-            } ${isVisible && `fade-in animate animation-forwards animation-delay-${(index + 1) * 2}00ms`}`}
-            id={`${card.title.toLowerCase()}`}>
-            <div className='m-25px'>
-              <h2 className='text-30px sm:text-25px'>{card.title}</h2>
-              <p className='text-15px mb-0px'>{card.description}</p>
-            </div>
-          </div>
-        ))}
+        <PresentationCard data={serviceCard} />
       </div>
     </Section>
   )
