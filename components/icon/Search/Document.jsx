36e8675ba79f457c33f1d9460d5141652995c953
Se-Gl <me@severinglaser.ch>
2022-04-21 00:57:10 +0200

    adjusted color of search result icons

--- components/icon/Search/Document.jsx
+++ components/icon/Search/Document.jsx
@@ -9,7 +9,7 @@ function Document({ className, width = '16', height = '20' }) {
       fill='none'
       viewBox='0 0 16 20'>
       <path
-        fill='#101010'
+        fill='#3e7a02'
         d='M0 2a2 2 0 012-2h8a1 1 0 01.707.293l5 5A1 1 0 0116 6v12a2 2 0 01-2 2H2a2 2 0 01-2-2V2zm13.586 4L10 2.414V6h3.586zM8 2H2v16h12V8H9a1 1 0 01-1-1V2zm-4 9a1 1 0 011-1h6a1 1 0 110 2H5a1 1 0 01-1-1zm0 4a1 1 0 011-1h6a1 1 0 110 2H5a1 1 0 01-1-1z'></path>
     </svg>
   )
