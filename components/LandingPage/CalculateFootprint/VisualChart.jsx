f0dd73e4fd7c8015853f97de8341c9933dfa1963
Se-Gl <me@severinglaser.ch>
2022-04-27 02:10:35 +0200

    refactored visual footprint chart

--- components/LandingPage/CalculateFootprint/VisualChart.jsx
+++ components/LandingPage/CalculateFootprint/VisualChart.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function VisualChart({ data }) {
   // initial values
   const a = Math.round(71.7)
@@ -10,6 +8,24 @@ export default function VisualChart({ data }) {
   const percenta = (a * 100) / result
   const percentb = (b * 100) / result
   const percentc = (c * 100) / result
+
+  const averageFootprint = [
+    {
+      description: 'Your PC/laptop footprint',
+      bgColor: 'bg-blue',
+      percent: percentc
+    },
+    {
+      description: 'PC/laptop footprint of an average person',
+      bgColor: 'bg-green',
+      percent: percentb
+    },
+    {
+      description: 'PC/laptop footprint of an average developer',
+      bgColor: 'bg-orange',
+      percent: percenta
+    }
+  ]
 
   return (
     <>
@@ -25,27 +41,16 @@ export default function VisualChart({ data }) {
         }}></div>
 
       <div className='w-100per flex'>
-        <div>
-          <div className='flex mt-25px'>
-            <div className='bg-blue h-10px sm:h-7px md:h-7px w-15px rounded-10px justify-center mt-7px mr-10px '></div>
-            <p className='mb-0px text-black-10 text-15px'>Your PC/laptop footprint</p>
+        {averageFootprint.map((average, index) => (
+          <div key={index}>
+            <div className='flex mt-25px'>
+              <div
+                className={`h-10px sm:h-7px md:h-7px w-15px rounded-10px justify-center mt-7px mr-10px ${average.bgColor}`}></div>
+              <p className='mb-0px text-black-10 text-15px'>{average.description}</p>
+            </div>
+            <p className='mb-0px text-white text-center'>{Math.round(average.percent)}%</p>
           </div>
-          <p className='mb-0px text-white text-center'>{Math.round(percentc)}%</p>
-        </div>
-        <div className='mx-25px'>
-          <div className='flex mt-25px'>
-            <div className='bg-green h-10px sm:h-5px md:h-7px w-15px rounded-10px justify-center mt-7px mr-10px '></div>
-            <p className='mb-0px text-black-10 text-15px'>PC/laptop footprint of an average person</p>
-          </div>
-          <p className='mb-0px text-white text-center'>{Math.round(percenta)}%</p>
-        </div>
-        <div>
-          <div className='flex mt-25px'>
-            <div className='bg-orange h-10px sm:h-7px md:h-7px w-15px rounded-10px justify-center mt-7px mr-10px '></div>
-            <p className='mb-0px text-black-10 text-15px'>PC/laptop footprint of an average developer</p>
-          </div>
-          <p className='mb-0px text-white text-center'>{Math.round(percentb)}%</p>
-        </div>
+        ))}
       </div>
     </>
   )
